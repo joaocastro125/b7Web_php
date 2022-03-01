bc57517f61f0a7a2ed9652be358bdde787ee6ef3
joao castro <joaocastro>
2022-02-28 21:44:22 -0300

    atualizando

--- variaveis.php
+++ variaveis.php
@@ -1,3 +1,10 @@
 <?php
-  
+   $name="joao";// string
+   $idade=33; //interger
+   $altura=1.80; // float 
+   $casado=true; // boleano 
+   $vazio=null; // vazio ou sem nenhum valor
+   $sobrenome=''; // string vazia 
+
+   echo $name;
 ?>
