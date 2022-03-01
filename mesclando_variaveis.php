ed38403096d454addef46e5a65fd303d3561bbc6
joao castro <joaocastro>
2022-03-01 19:38:00 -0300

    atualizando

---
+++ mesclando_variaveis.php
@@ -0,0 +1,15 @@
+<?php
+   $name="joao";// string
+   $idade=33; //interger
+   $altura=1.80; // float 
+   $casado=true; // boleano 
+   $vazio=null; // vazio ou sem nenhum valor
+   $sobrenome='castro'; // string vazia 
+
+   echo $name.' '.$sobrenome."<br>"; // varios tipos de execultar variaveis 
+
+   echo "$name $sobrenome <br>";
+   echo $name.=" $sobrenome "; // uma forma reduzida de concatenar 
+
+
+?>
